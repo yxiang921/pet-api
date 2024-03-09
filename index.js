ec5a14f1a4359218dd113614077ab3ae0f563643
yxiang921 <xiangf35@gmail.com>
2024-03-09 13:17:11 +0800

    Update Files

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -9,9 +8,6 @@ import appointmentRoutes from "./routes/appointment.routes.js"
 import petRoutes from "./routes/pet.routes.js"
 
 import conn from "./db/conn.js";
-
-const __dirname = path.resolve();
-
 
 dotenv.config();
 
@@ -26,11 +22,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/reminder", reminderRoutes);
 app.use("/api/appointment",appointmentRoutes);
 app.use("/api/pet", petRoutes);
-
-app.use(express.static(path.join(__dirname, "client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
